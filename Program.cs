5b7ee0d07601d7e89397fdd0fb0f2b5134fb7a15
andrey <ai.poteryaev@gmail.com>
2023-01-11 16:18:17 +0500

    Final task

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// // Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// // длина которых меньше либо равна 3 символам. Первоночальный массив можно ввести с клавиатуры, 
+// // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться
+// // колекциями, лучше обойтись исключительно массивами.
+
+
+using static System.Console;
+Clear();
+
+string[] array1 = new string[6] {"window", "dog", "hey", "world", "cat", "peace"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Write($"{array[i]} ");
+    }
+    WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
